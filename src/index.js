70cacbb525167577976d839f4c2e7f2b6d941707
Kevin Zheng <kevzheng27@gmail.com>
2022-06-28 21:39:58 -0400

    commit tree a81fede38e2fa4b863b8ed0cd38e225aea360a28

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const toyDiv = document.getElementById('toy-collection');
+const form = document.querySelector('form');
+const url = 'http://localhost:3000/toys';
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +17,99 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', getToys)
+
+function getToys() {
+  fetch(url)
+  .then(resp => resp.json())
+  .then(toyObj => {
+    buildCard(toyObj)
+    globalToyObj = toyObj
+  })
+}
+
+function buildCard(toyObj) {
+  for(const toy of toyObj) {
+    let card = document.createElement('div');
+    card.className = 'card'
+    // uses interpolation below
+    card.innerHTML = `                
+      <h2>${toy.name}</h2> 
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn" id=${toy.id}>Like ❤️</button>
+    `
+    toyDiv.appendChild(card)
+
+    const likeButton = document.getElementById(`${toy.id}`)
+    likeButton.addEventListener('click', function() {
+      toy.likes++
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: 'PATCH', 
+      headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'},
+      body: JSON.stringify({
+        'likes': `${toy.likes}`
+      })
+    } )
+    .then(resp => resp.json())
+    .then(updateDOM => {
+      const p = document.getElementById(`${toy.id}`).previousElementSibling
+      p.textContent = `${updateDOM.likes} Likes` 
+    })
+  }
+)
+}}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const toyNameInput = e.target.name.value
+  const toyImageInput = e.target.image.value
+  
+  fetch(url, {
+    method: 'POST',
+          headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',},
+          body: JSON.stringify({
+            "name": toyNameInput,
+            "image": toyImageInput,
+            "likes": 0
+          })
+  })
+  .then(resp => resp.json())
+  .then(addedToy => {
+    let newCard = document.createElement('div');
+    newCard.className = 'card'
+  // uses interpolation below
+    newCard.innerHTML = `                
+    <h2>${addedToy.name}</h2> 
+    <img src=${addedToy.image} class="toy-avatar" />
+    <p>${addedToy.likes} Likes </p>
+    <button class="like-btn" id=${addedToy.id}>Like ❤️</button>
+  `
+  toyDiv.appendChild(newCard)  //an image will not show right away when image is submitted, it will only show when refreshed because the database has been updated but we want the image to show right away so we'll need to add to dom right away 
+  
+  const newLikeButton = document.getElementById(`${addedToy.id}`)
+  newLikeButton.addEventListener('click', function() {
+    addedToy.likes++
+    fetch(`http://localhost:3000/toys/${addedToy.id}`, {
+    method: 'PATCH', 
+    headers:{
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'},
+    body: JSON.stringify({
+      'likes': `${addedToy.likes}`
+    })
+  } )
+  .then(resp => resp.json())
+  .then(updateDOMnew => {
+    const p = document.getElementById(`${addedToy.id}`).previousElementSibling
+    p.textContent = `${updateDOMnew.likes} Likes` 
+  })
+}
+)  
+})
+})
